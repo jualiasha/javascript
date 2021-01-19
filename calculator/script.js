a77235450ef074bccad217d73fabd1702774bde1
Matviishyna Julia <s2100159@edu.bc.fi>
2021-01-19 14:48:48 +0200

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,12 @@
+let x,y;
+/* x=prompt("Give me please a number");
+
+
+y=prompt("Give me second number");
+
+alert("Sum for those number is " +(Number(x)+Number(y))); */
+
+x=prompt("What age are you?");
+if (x<=18) {alert("You are too young!")};
+if (x>18) {alert("Welcome")};
+
